f5d3c94f670a3deff0370422b8f1297b448312d7
guilherme <guilherme.santos@dellead.com>
2023-04-16 11:29:38 -0300

    added mongodb connection and finished crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
